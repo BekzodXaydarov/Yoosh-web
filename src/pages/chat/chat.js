f30605f2c2261b24cf1a67b34d4266d1e7338a70
Takitok2011 <bekzodeoke@gmail.com>
2024-04-04 01:01:36 +0300

    h

--- src/pages/chat/chat.js
+++ src/pages/chat/chat.js
@@ -1,8 +1,34 @@
 import React from 'react'
 import './chat.css'
+import * as i from '../../assets/svgs/index'
 
 export default function Chat() {
   return (
-    <div>Chat</div>
+    <div className='Chat'>
+      <nav className='chat_nav'>
+        <div className="chat_nav_left">
+          <i.Chat />
+         <h1>Chat</h1>
+         <i.Check_down />
+        </div>
+        <div className="chat_nav_right">
+          <i.Search_icon  />
+          <i.Framed />
+        </div>
+      </nav>
+      <div className="chat_body"></div>
+      <div className="chat_footer">
+        <div className="chat_footer_left">
+          <i.Check />
+          <i.File />
+        </div>
+        <div className="chat_footer_between">
+          <input type="text" placeholder='Start typing' />
+        </div>
+        <div className="chat_footer_right">
+          <i.Send />
+        </div>
+      </div>
+    </div>
   )
 }
