92070ad050918dae05efd97dfc4de02bd3861482
Takitok2011 <bekzodeoke@gmail.com>
2024-04-08 19:59:14 +0300

    m

--- src/components/Navbar_compoent/index.js
+++ src/components/Navbar_compoent/index.js
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavCard from '../nav_card/nav_card'
+import Modal from '../modal/index'
 
 export default function NavbarComponent({children}) {
     const children_head = children[0]?.children[0]
     const children_body = children[1]?.children
+    const [open,setopen] = useState(false)
     return (
         <div key={children.key}>
             <div className="nav_head">
                 <h1>{children_head.title}</h1>
-                {children_head.svg}
+                <button onClick={()=>setopen(!open)}>{children_head.svg}</button>
             </div>
+            {
+            open ? <Modal /> : null
+            }
             <div className="nav_body">
                 {
                     children_body.map((item) => {
--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import * as i from '../../assets/svgs/index'
 import './modal.css'
 
 export default function Modal() {
   return (
     <div className='modal'>
-      Modal
+      <p>Add channels</p>
+      <ul>
+        <li><i.Create /><h4>Create new channel</h4></li>
+        <li><i.Create /><h4>Create new channel</h4></li>
+        <li><i.Create /><h4>Create new channel</h4></li>
+        <li><i.Create /><h4>Create new channel</h4></li>
+      </ul>
     </div>
   )
 }
