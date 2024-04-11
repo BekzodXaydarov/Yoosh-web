c5b3fe5fbe3d787be4c65d56d6da7c431a4baf3f
Takitok2011 <bekzodeoke@gmail.com>
2024-04-11 20:36:30 +0500

    m

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -11,13 +11,6 @@ export default function Modal() {
         <li><i.CreateFilter /><h4>Create new filter</h4></li>
         <li><i.Add /><h4>Add existing channels</h4></li>
         <li><i.Manage /><h4>Manage channels</h4></li>
-        <p>Add channels</p>
-        <ul>
-          <li><i.Create /><h4>Create new channel</h4></li>
-          <li><i.Create /><h4>Create new channel</h4></li>
-          <li><i.Create /><h4>Create new channel</h4></li>
-          <li><i.Create /><h4>Create new channel</h4></li>
-        </ul>
         </ul>
     </div>
   )
